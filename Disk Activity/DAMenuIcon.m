29732be69b393a0006025a416e7019671f011e1f
melchor629 <melchor9000@gmail.com>
2013-11-24 22:04:28 +0100

    Dynamic Graph Icon
    Now the icon is an image dynamic created. It needs to update the size
    adapting the options

--- Disk Activity/DAMenuIcon.m
+++ Disk Activity/DAMenuIcon.m
@@ -59,6 +59,7 @@ void getDISKcounters(io_iterator_t drivelist, io_s *io_s) {
 io_s io;
 io_iterator_t drivelist  = IO_OBJECT_NULL;
 mach_port_t masterPort = IO_OBJECT_NULL;
+int quitI, preferencesI, iconI, textI, graphI;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     
@@ -80,10 +81,10 @@ mach_port_t masterPort = IO_OBJECT_NULL;
     [text setTarget:self]; [text setState:NSOnState];
     //Alloc and init Menu and fill with menu items
     menu = [[NSMenu alloc] initWithTitle:@"Disk Activity"];
-    [menu insertItem:icon atIndex:index++];
-    [menu insertItem:text atIndex:index++];
-    [menu insertItem:preferences atIndex:index++];
-    [menu insertItem:quit atIndex:index++];
+    [menu insertItem:icon atIndex:index]; iconI = index++;
+    [menu insertItem:text atIndex:index]; textI = index++;
+    [menu insertItem:preferences atIndex:index]; preferencesI = index++;
+    [menu insertItem:quit atIndex:index]; quitI = index++;
 
     //Add the item to Status Bar
     statusItem = [[NSStatusBar systemStatusBar]
@@ -95,11 +96,11 @@ mach_port_t masterPort = IO_OBJECT_NULL;
     [statusItem setTarget:self];
 
     //Text and Image
-    _IconOff = [NSImage imageNamed:@"MenuBarIconOff"];
+    /*_IconOff = [NSImage imageNamed:@"MenuBarIconOff"];
     _IconGreen = [NSImage imageNamed:@"MenuBarIconGreen"];
     _IconRed = [NSImage imageNamed:@"MenuBarIconRed"];
     _IconBoth = [NSImage imageNamed:@"MenuBarIconBoth"];
-    [statusItem setImage:_IconOff];
+    [statusItem setImage:_IconOff];*/
 
     //Timer to update information
     updateTimer = [NSTimer
@@ -124,38 +125,80 @@ mach_port_t masterPort = IO_OBJECT_NULL;
 
 - (IBAction)updateDiskUsage:(id)sender {
     getDISKcounters(drivelist, &io);
-    NSMutableString *title = [[NSMutableString alloc] init];
+    //NSMutableString *title = [[NSMutableString alloc] init];
+    NSString *readS = [NSString alloc], *writeS = [NSString alloc];
+    NSImage *anImage = [[NSImage alloc] initWithSize:NSMakeSize(60.0, 21.0)];
 
     if(_text) {
-        if(io.ispeed < 1000)
+        /*if(io.ispeed < 1000)
             [title appendFormat:@"%lliB/s", io.ispeed];
         else if(io.ispeed/1000 < 1000)
             [title appendFormat:@"%lliKB/s", io.ispeed/1000];
         else if(io.ispeed/1000/1000 < 1000)
-            [title appendFormat:@"%lliMB/s", io.ispeed/1000/1000];
+            [title appendFormat:@"%.1fMB/s", io.ispeed/1000.0/1000.0];
         [title appendString:@" - "];
         if(io.ospeed < 1000)
             [title appendFormat:@"%lliB/s", io.ospeed];
         else if(io.ospeed/1000 < 1000)
             [title appendFormat:@"%lliKB/s", io.ospeed/1000];
         else if(io.ospeed/1000/1000 < 1000)
-            [title appendFormat:@"%lliMB/s", io.ospeed/1000/1000];
+            [title appendFormat:@"%.1fMB/s", io.ospeed/1000.0/1000.0];*/
+        if(io.ispeed < 1000)
+            readS = [readS initWithFormat:@"%lliB/s", io.ispeed];
+        else if(io.ispeed/1000 < 1000)
+            readS = [readS initWithFormat:@"%lliKB/s", io.ispeed/1000];
+        else if(io.ispeed/1000/1000 < 1000)
+            readS = [readS initWithFormat:@"%.1fMB/s", io.ispeed/1000.0/1000.0];
+
+        if(io.ospeed < 1000)
+            writeS = [writeS initWithFormat:@"%lliB/s", io.ospeed];
+        else if(io.ospeed/1000 < 1000)
+            writeS = [writeS initWithFormat:@"%lliKB/s", io.ospeed/1000];
+        else if(io.ospeed/1000/1000 < 1000)
+            writeS = [writeS initWithFormat:@"%.1fMB/s", io.ospeed/1000.0/1000.0];
+        [anImage lockFocus];
+        NSFont *f = [NSFont fontWithName:@"Lucida Grande" size:9.0];
+        NSDictionary* attributes = [NSDictionary dictionaryWithObjectsAndKeys:f,
+                                    NSFontAttributeName, [NSColor colorWithRed:10.0/255.0 green:180/255.0 blue:15/255.0 alpha:1],
+                                    NSForegroundColorAttributeName, nil];
+        [readS drawAtPoint:NSMakePoint(16, 10) withAttributes:attributes];
+        attributes = [NSDictionary dictionaryWithObjectsAndKeys:f,
+                      NSFontAttributeName, [NSColor colorWithRed:255/255.0 green:10/230.0 blue:15/255.0 alpha:1],
+                      NSForegroundColorAttributeName, nil];
+        [writeS drawAtPoint:NSMakePoint(16, 0) withAttributes:attributes];
+        [anImage unlockFocus];
+        [statusItem setImage:anImage];
     }
 
-    [statusItem setTitle:title];
+    //[statusItem setTitle:title];
     if(_icon) {
-        if(io.ispeed > 1000 && io.ospeed < 1000)
+        /*if(io.ispeed > 1000 && io.ospeed < 1000)
             [statusItem setImage:_IconGreen];
         else if(io.ispeed < 1000 && io.ospeed > 1000)
             [statusItem setImage:_IconRed];
         else if(io.ispeed > 1000 && io.ospeed > 1000)
             [statusItem setImage:_IconBoth];
         else if(![[statusItem image] isEqualTo:_IconOff])
-            [statusItem setImage:_IconOff];
-    } else {
+            [statusItem setImage:_IconOff];*/
+        float read = io.ispeed / 1000.0 * 18 / 2000.0;
+        float write = io.ospeed / 1000.0 * 18 / 2000.0;
+        read = read > 18.0 ? 18.0 : read;
+        write = write > 18.0 ? 18.0 : write;
+
+        [anImage lockFocus];
+        [[NSColor colorWithRed:100/255.0 green:100/255.0 blue:100/255.0 alpha:0.3] setFill];
+        [[NSBezierPath bezierPathWithRect:NSMakeRect(2, 1, 5, 18)] fill];
+        [[NSBezierPath bezierPathWithRect:NSMakeRect(9, 1, 5, 18)] fill];
+        [[NSColor colorWithRed:50.0/255.0 green:255/255.0 blue:55/255.0 alpha:1] setFill];
+        [[NSBezierPath bezierPathWithRect:NSMakeRect(2, 1, 5, read)] fill];
+        [[NSColor colorWithRed:255/255.0 green:50/255.0 blue:55/255.0 alpha:1] setFill];
+        [[NSBezierPath bezierPathWithRect:NSMakeRect(9, 1, 5, write)] fill];
+        [anImage unlockFocus];
+        [statusItem setImage:anImage];
+    }/* else {
         if([statusItem image] != nil)
             [statusItem setImage:nil];
-    }
+    }*/
 }
 
 - (IBAction)quit:(id)sender {
@@ -163,18 +206,18 @@ mach_port_t masterPort = IO_OBJECT_NULL;
 }
 
 - (IBAction)preferences:(id)sender {
-    
+
 }
 
 - (IBAction)showHideIcon:(id)sender {
     _icon = !_icon;
     if(_icon)
-        [[menu itemAtIndex:0] setState:NSOnState];
+        [[menu itemAtIndex:iconI] setState:NSOnState];
     else if(!_icon && _text)
-        [[menu itemAtIndex:0] setState:NSOffState];
+        [[menu itemAtIndex:iconI] setState:NSOffState];
     else if(!_icon && !_text) {
-        [[menu itemAtIndex:0] setState:NSOffState];
-        [[menu itemAtIndex:1] setState:NSOnState];
+        [[menu itemAtIndex:iconI] setState:NSOffState];
+        [[menu itemAtIndex:textI] setState:NSOnState];
         _text = true;
     }
 }
@@ -185,12 +228,12 @@ mach_port_t masterPort = IO_OBJECT_NULL;
         [menu setTitle:@""];
     else if(!_text && !_icon) {
         _icon = true;
-        [[menu itemAtIndex:0] setState:NSOnState];
+        [[menu itemAtIndex:iconI] setState:NSOnState];
     }
     if(_text)
-        [[menu itemAtIndex:1] setState:NSOnState];
+        [[menu itemAtIndex:textI] setState:NSOnState];
     else
-        [[menu itemAtIndex:1] setState:NSOffState];
+        [[menu itemAtIndex:textI] setState:NSOffState];
 }
 
 @end
